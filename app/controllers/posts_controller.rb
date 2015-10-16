e8a8f097fa6d455b7fd51a85f20335e2553031e8
Student <none@no.one>
2015-10-16 10:30:01 -0700

    can now add posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,6 +4,29 @@ class PostsController < ApplicationController
   end
 
   def show
+    @posts = Post.all
     @post = Post.find(params[:id])
   end
+
+  def new
+    @posts = Post.all
+    @post = Post.new
+  end
+
+  def create
+    @posts = Post.all
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to  posts_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+    def post_params
+      params.require(:post).permit(:author, :title, :content)
+    end
+
 end
